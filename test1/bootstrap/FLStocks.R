3979a0d77698afc2f332e132c6de6d87a08029ce
Colin Millar <colin.millar@ices.dk>
2020-02-07 09:45:30 +0100

    just save all stocks for use

--- test1/bootstrap/FLStocks.R
+++ test1/bootstrap/FLStocks.R
@@ -4,9 +4,4 @@ taf.library(FLCore)
 
 data(icesStocks)
 
-lapply(icesStocks, range)
-
-# make a stock with recruitment at age 5
-recage5 <- icesStocks$saiIC[paste(5:14), ]
-
-saveRDS(recage5, file = "recage5.rds")
+saveRDS(icesStocks, file = "icesStocks.rds")
